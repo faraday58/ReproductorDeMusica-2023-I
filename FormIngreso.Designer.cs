8038cd46726b0f4a4b196c1ea2b514bf699f1d66
Alumnos <Alumnos@CONTROL3-PC>
2022-11-09 12:27:23 -0600

    Agregar archivos de proyecto.

---
+++ FormIngreso.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ReproductorDeMusica
+{
+    partial class FormIngreso
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtbUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbContra = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(128, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario";
+            // 
+            // txtbUsuario
+            // 
+            this.txtbUsuario.Location = new System.Drawing.Point(219, 65);
+            this.txtbUsuario.Name = "txtbUsuario";
+            this.txtbUsuario.Size = new System.Drawing.Size(190, 20);
+            this.txtbUsuario.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(128, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Contraseña";
+            // 
+            // txtbContra
+            // 
+            this.txtbContra.Location = new System.Drawing.Point(219, 118);
+            this.txtbContra.Name = "txtbContra";
+            this.txtbContra.PasswordChar = '*';
+            this.txtbContra.Size = new System.Drawing.Size(190, 20);
+            this.txtbContra.TabIndex = 2;
+            this.txtbContra.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtbContra_KeyPress);
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(242, 173);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(138, 42);
+            this.btnIngresar.TabIndex = 3;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // FormIngreso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 263);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txtbContra);
+            this.Controls.Add(this.txtbUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormIngreso";
+            this.Text = "Ingreso";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtbUsuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbContra;
+        private System.Windows.Forms.Button btnIngresar;
+    }
+}
+
---
+++ FormIngreso.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Reproductor;
+using System.Windows.Forms;
+using Usuarios;
+
+namespace ReproductorDeMusica
+{
+    public partial class FormIngreso : Form
+    {
+        private string usuario;
+        private string password;
+        private Coleccion cUsuarios;
+      //  private Usuario Usuario;
+
+        public FormIngreso()
+        {
+            InitializeComponent();
+            cUsuarios = new Coleccion();
+        //    Usuario = new Usuario();
+        //  Usuario.NomUsuario = "";
+        //  Usuario.Password = "";
+            
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+
+            Valida();
+
+        }
+
+        private void txtbContra_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if( e.KeyChar == (char)Keys.Enter  )
+            {
+                Valida();
+            }
+            
+        }
+
+        public void Valida()
+        {
+            string usuario = txtbUsuario.Text;
+            string password = txtbContra.Text;
+
+            //    txtbUsuario.Text = Usuario.NomUsuario;
+            //   txtbContra.Text = Usuario.Password;
+
+            for (int i = 0; i < 2; i++)
+            {
+                Usuario miUsuario = new Usuario();
+                miUsuario =(Usuario)cUsuarios.usuarios[i];
+                this.usuario = miUsuario.NomUsuario;
+                this.password = miUsuario.Password;
+
+
+                if (this.usuario == usuario && this.password == password)
+                {
+                    //  MessageBox.Show("Ingreso Exitoso");
+                  /*  FormReproductor formReproductor = new FormReproductor();
+                    formReproductor.Show();
+                  */
+                    FormListaMusica formListaMusica = new FormListaMusica();
+                    formListaMusica.Show();
+                    break;
+                }
+                if( i== 1  )
+                {
+                    MessageBox.Show("Usuario o Contraseña incorrecto  ");
+                }
+            }
+        }
+    }
+}
